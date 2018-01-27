36ac1c7bb00cf7588794b2dd69b38409a202b486
sloba82 <dizajnistampa@gmail.com>
2018-01-27 22:32:40 +0100

    pozelji_forma_change
    izmena na formi

--- strane/mail.php
+++ strane/mail.php
@@ -6,11 +6,3 @@ $mesto = $_POST['mesto'];
 $mail = $_POST['email'];
 $tel = $_POST['tel'];
 $napomena = $_POST['napomena'];
-
-function poruka() {
-echo "ime:".$ime."<br>"."ulica: ".$ulica."<br>"."mesto: ".$mesto."<br>"."email: ".$mail."<br>".
-"telefon: ".$tel."<br>"."napomena: ".$napomena;
-}
-
-echo poruka();
-
