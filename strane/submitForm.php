f1859eb950a78fafeb2e36fd7314429d407cbc69
sloba82 <dizajnistampa@gmail.com>
2018-02-04 03:42:03 +0100

    finished_sandmail_with_delete_file
    zavrsen sendamail i brisanje uplodovanog fiala

--- strane/submitForm.php
+++ strane/submitForm.php
@@ -11,122 +11,114 @@ date_default_timezone_set('Etc/UTC');
 
 require '../PHPMailer/PHPMailerAutoload.php';
 
+$urlprezent = $_POST['urlprezent'];
+$urlprezent = strtok($urlprezent, '?');
 
+if (  $_POST['name'] != '' ){
 
- $urlprezent = $_POST['urlprezent'];
+        getPostAndSendMail();
+        header('Location: '.$urlprezent.'?message=susses');
 
-
-$message;
-foreach($_POST as $key => $value) {
-    if ($value == '') {
-        $value = "NODATA";
-    }
-    $message .= $key.": "." ".$value."<br>";
+}else {
+        header('Location: '.$urlprezent.'?message=no_susses');
+        die();
 }
+ 
+    function getPostAndSendMail() {   
 
-    $img = $_FILES['fileToUpload']['name'];
+            $message;
+            foreach($_POST as $key => $value) {
+                if ($value == '') {
+                    $value = "NODATA";
+                }
+                $message .= $key.": "." ".$value."<br>";
+            }
+             $img = $_FILES['fileToUpload']['name'];
 
-    // name for image
-    $name = trim($_POST['name']);
-    $name = str_replace(' ', '_', $name);
-    $name .= time();
+                // name for image
+                $name = trim($_POST['name']);
+                $name = str_replace(' ', '_', $name);
+                $name .= time();
+             
+                $imgname = imgupload($img, $name);
+                if (sendMail($message, $imgname) == 1) {
+                    deletefile($imgname);
+                }
+                    
 
 
-
-    $imgname = imgupload($img, $name, $urlprezent );
-
-     sendMail($message, $urlprezent, $imgname);
+    }        
 
 
+    function imgupload($img, $name){
+        $info = pathinfo($img);
+        $ext = $info['extension']; // get the extension of the file
+        $newname = $name .".". $ext;
 
+        $target = '../upload/' . $newname;
+        move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target);
 
-function imgupload ($img, $name, $urlprezent ){
-    $info = pathinfo($img);
-    $ext = $info['extension']; // get the extension of the file
-    $newname = $name .".". $ext;
-
-    $target = '../upload/' . $newname;
-    move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target);
-
-    if ($ext == 'jpg' || $ext == 'png' || $ext == 'jpeg' || $ext == 'pdf'  ) {
         return $newname;
-    }else {
-        header('Location: '.$urlprezent);
+      
     }
 
-}
+    function deletefile($imgname){
 
-function deletefile($imgname){
+     /*  fclose("../upload/".$imgnam);
+        unlink("../upload/".$imgnam);*/
+          
+            sleep(2);
 
-    $files = glob('../upload/'.$imgname);
+            $path = '../upload/'.$imgname;
+            if(file_exists($path) && is_readable($path)) {
+                unlink($path);
+            }
 
-        if(is_file($files)) {
-            unlink('../upload/'.$imgname);
+     
+    }
+
+
+
+    function sendMail($message, $imgname )
+    {
+
+        $mail = new PHPMailer;
+        $mail->isSMTP();
+
+        $mail->SMTPDebug = 0; //Enable SMTP debugging, 0 = off (for production use), 1 = client messages, 2 = client and server messages
+        $mail->Debugoutput = 'html';
+
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 587;
+        $mail->SMTPSecure = 'tls';
+        $mail->SMTPAuth = true;
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+        $mail->Username = "dizajnistampa@gmail.com";
+        $mail->Password = "82slobadragana";
+        $mail->setFrom('dizajnistampa@gmail.com', 'web');
+        $mail->addAddress('dizajnistampa@gmail.com', 'print');
+        $mail->Subject = 'Fotoplatna poruka';
+        $mail->msgHTML($message);
+        $mail->Body = $message;
+        $atacment = '../upload/'. $imgname;
+        $mail->addAttachment($atacment);
+
+        if (!$mail->send()) {
+            echo "message  not sent";
+        } else {
+            return 1;;
+
         }
 
-
-}
-
-
-
-function sendMail($message, $urlprezent, $imgname )
-{
-
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-
-    $mail->SMTPDebug = 0; //Enable SMTP debugging, 0 = off (for production use), 1 = client messages, 2 = client and server messages
-    $mail->Debugoutput = 'html';
-
-    $mail->Host = 'smtp.gmail.com';
-    $mail->Port = 587;
-    $mail->SMTPSecure = 'tls';
-    $mail->SMTPAuth = true;
-    $mail->SMTPOptions = array(
-        'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true
-        )
-    );
-
-    $mail->Username = "dizajnistampa@gmail.com";
-    $mail->Password = "82slobadragana";
-    $mail->setFrom('dizajnistampa@gmail.com', 'web');
-
-//Set who the message is to be sent to
-    $mail->addAddress('dizajnistampa@gmail.com', 'print');
-
-//Set the subject line
-    $mail->Subject = 'Fotoplatna poruka';
-
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-    $mail->msgHTML($message);
-
-
-    $mail->Body = $message;
-
-//Attach an image file
-
-    $atacment = '../upload/'. $imgname;
-
-    $mail->addAttachment($atacment);
-
-//send the message, check for errors
-
-
-
-
-    if (!$mail->send()) {
-        echo "mesa not send";
-    } else {
-       echo "mess sent";
-
     }
 
-  return header('Location: '.$urlprezent);
 
 
-}
 
+      
